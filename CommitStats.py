82829eda070706b0565f77689568f889f4f71e6a
Nathan Mador-House <nathan@musicnate.ca>
2016-09-11 23:52:24 -0700

    Middle of changing to OOP.

--- CommitStats.py
+++ CommitStats.py
@@ -24,44 +24,53 @@ import re
 # 2. FUNCTIONS
 ###################################################################
 
+class user():
 
-def make_html(url):
-    """ Make the html doc """
-    html = urllib.request.Request(url)
-    response = urllib.request.urlopen(html)
-    return response.read().decode('utf-8')
+    """ A user object """
 
-
-def make_soup(html):
-    """ Make the soup """
-    the_soup = BeautifulSoup(html, 'html.parser')
-    return the_soup
-
-
-def get_total_commits(soup):
-    """ Get the easy number """
-    total = soup.find_all("h3")
-    return re.sub("\D", "", total[1].string)
+    def __init__(self, url):
+        """ Init """
+        self.url = url
+        self.soup = self.make_soup()
+        self.total_commits = 
+        self.highest_commits = 
+        self.average_commits = 
 
 
-def get_all_days(soup):
-    """ Get all the data for each day tag """
-    date_count = []
-    days = soup.find_all("rect")
-    for i in range(0, len(days)):
-        counter = re.search('data-count="."', str(days[i]))
-        number = int(re.sub("\D", "", counter.group(0)))
-        date_count.append(number)
-    return date_count
+    def print_stats(self):
+        """ Print user stats """
+        print(self.total_commits)
+        print(self.highest_commits)
+        print(self.average_commits)
 
 
-def count_math(count_list):
-    total = sum(count_list)
-    average = round(total/len(count_list), 3)
-    highest = max(count_list)
-    print("Total commits: " + str(total))
-    print("Highest commits in single day: " + str(highest))
-    print("Average commits per day: " + str(average))
+    def make_soup(self):
+        """ Make the html doc """
+        html = urllib.request.Request(self.url)
+        response = urllib.request.urlopen(html)
+        html_response = response.read().decode('utf-8')
+        the_soup = BeautifulSoup(html_response, 'html.parser')
+        return the_soup
+
+
+    def get_all_days(self):
+        """ Get all the data for each day tag """
+        date_count = []
+        days = self.soup.find_all("rect")
+        for i in range(0, len(days)):
+            counter = re.search('data-count="."', str(days[i]))
+            number = int(re.sub("\D", "", counter.group(0)))
+            date_count.append(number)
+        return date_count
+
+
+    def count_math(self):
+        total = sum(count_list)
+        average = round(total/len(count_list), 3)
+        highest = max(count_list)
+        print("Total commits: " + str(total))
+        print("Highest commits in single day: " + str(highest))
+        print("Average commits per day: " + str(average))
 
 ###################################################################
 # 3. MAIN
